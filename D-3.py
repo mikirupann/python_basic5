d666e6f9e3defdbf85b8c8596fd363f8df64dc0c
mikirupann <sw.miki.0907@icloud.com>
2022-01-27 23:38:45 +0900

    D-3完了
    お願いします

---
+++ D-3.py
@@ -0,0 +1,21 @@
+import math
+
+
+class Square:
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+    def diagonal(self):
+        return format((self.side * (math.sqrt(2))), '.2f')
+
+
+square1 = Square(side=1.5)
+print(square1.area())  # 2.25
+print(square1.diagonal())  # 2.12
+
+square2 = Square(side=15)
+print(square2.area())  # 225
+print(square2.diagonal())  # 21.21
