5e9cb42bf1d0d6dd0d0c75437f4a50da54c2f553
mikirupann <sw.miki.0907@icloud.com>
2022-01-28 00:42:22 +0900

    D-6完了
    頑張った！！

--- D-6.py
+++ D-6.py
@@ -1,13 +1,13 @@
-class MyCounter:
+class MyCounterV3:
     def __init__(self, value, step):
         self.value = value
         self.step = step
 
     def count_up(self):
-        self.value = self.value + 2
+        self.value = self.value + self.step
 
     def count_down(self):
-        self.value = self.value - 2
+        self.value = self.value - self.step
 
 
 counter1 = MyCounterV3(value=1, step=2)
